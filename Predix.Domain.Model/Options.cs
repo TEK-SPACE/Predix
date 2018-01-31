40b246e773b6e6b14a65a7168f6df128637b66c8
Harsha Bopuri <hbopuri@hotmail.com>
2018-01-31 16:03:09 +0530

    command line arguments

--- Predix.Domain.Model/Options.cs
+++ Predix.Domain.Model/Options.cs
@@ -5,15 +5,15 @@ namespace Predix.Domain.Model
 {
     public class Options
     {
-        [Option('l', "loc", Required = true,
+        [Option('l', "loc", //Required = true,
             HelpText = "RefreshLocations.")]
         public bool RefreshLocations { get; set; }
 
-        [Option('i', "irc", Required = true,
+        [Option('i', "irc", //Required = true,
             HelpText = "Ignore Regulation Check.")]
         public bool IgnoreRegulationCheck { get; set; }
 
-        [Option('e', "se", Required = true,
+        [Option('e', "se", //Required = true,
             HelpText = "Save Events.")]
         public bool SaveEvents { get; set; }
 
